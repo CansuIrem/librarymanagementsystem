b45d4e00f8946c336b8bb25f27c4e7d2f855396b
Cansu İrem Kanlı <ciremkanli@icloud.com>
2025-05-04 16:25:50 +0300

    Initial commit

---
+++ src/main/java/com/cansuiremkanli/libmanage/LibmanageApplication.java
@@ -0,0 +1,13 @@
+package com.cansuiremkanli.libmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibmanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibmanageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cansuiremkanli/libmanage/LibmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cansuiremkanli.libmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibmanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
